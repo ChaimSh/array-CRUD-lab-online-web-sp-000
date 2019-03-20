75a38129d8f004ce29590b0cf9c7f6200fce4d1d
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-20 02:26:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-fruits[apples, oranges, bananas, pineapples]
+[apples, oranges, bananas, pineapples]
 end
 
 def add_element_to_end_of_array(array, element)

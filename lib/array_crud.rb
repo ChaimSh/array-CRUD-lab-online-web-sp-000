6161fafa401e885d2b43dd08467131bb08e9ea48
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-20 02:41:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,11 +22,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  fruits = ["apples", "oranges", "bananas", "pineapples"]
+apples = fruits.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  fruits = ["apples", "oranges", "bananas", "pineapples"]
+  fruits[3]
 end
 
 def retrieve_first_element_from_array(array)

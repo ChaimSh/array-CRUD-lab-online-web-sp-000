99cfce8df30129ccc4f6f99e1c0d5547884f67a9
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-20 02:31:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-fruits = ["apples", "oranges", "bananas", "pineapples"] 
+fruits = ["apples", "oranges", "bananas", "pineapples"]
 fruits << "kiwi"
 end
 

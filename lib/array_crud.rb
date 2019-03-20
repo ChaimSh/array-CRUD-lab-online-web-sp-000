2c2c56c7590dc7f233995c2d683a06a5ab855237
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-20 02:38:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  fruits = ["apples", "oranges", "bananas", "pineapples"]
+  fruits.pop
 end
 
 def remove_element_from_start_of_array(array)
